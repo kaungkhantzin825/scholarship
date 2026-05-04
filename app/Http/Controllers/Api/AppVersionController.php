edcb0e0aca4e801080b447f122084ec3047f8f28
kaungkhantzin <webdeveloperkkz@gmail.com>
2026-05-04 12:49:27 +0800

    Fix: Remove duplicate Controller import in AppVersionController

--- app/Http/Controllers/Api/AppVersionController.php
+++ app/Http/Controllers/Api/AppVersionController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Controller;
 use App\Models\AppVersion;
 use Illuminate\Http\Request;
 
@@ -12,6 +11,7 @@ class AppVersionController extends Controller
     public function index()
     {
         $settings = AppVersion::firstOrCreate(['id' => 1]);
+        
         return response()->json([
             'success' => true,
             'data' => $settings
